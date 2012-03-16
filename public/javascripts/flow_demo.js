fa80b72c4eee1955b308ada8fec8a348450d61c3
Jesse Smith <jesses16@gmail.com>
2012-03-16 11:05:03 -0700

    Updates to Video Demo

--- public/javascripts/flow_demo.js
+++ public/javascripts/flow_demo.js
@@ -12,11 +12,17 @@ flowplayer("player", "/flash/flowplayer-3.2.7.swf", {
           // when playback is stopped clear timer
           //if (self.getState() == 5) clearInterval(timer);
         //}, 1000);
-		scene_count = 0
+		scene_count = 0;
+		prev_cuepoint = 0;
           
         $f().onCuepoint(
           getCuepoints($f().getClip(0)),
           function(clip, cuepoint) {
+			//console.log ("cue - prev_cue = " + (cuepoint - prev_cuepoint));
+			if ( ( (cuepoint - prev_cuepoint) > 1000) || ( (cuepoint - prev_cuepoint) < 0 ) ) {
+				setDemoTextToTime(cuepoint);
+			}
+			prev_cuepoint = cuepoint;
             //This will act every second of video, with the variable "cuepoint" holding the current number of millis
 			console.log("at cuepoint: " + cuepoint);
 			//console.log(window.video_metadata);
@@ -32,21 +38,37 @@ flowplayer("player", "/flash/flowplayer-3.2.7.swf", {
 			
 			if ( ("elem_" + (cuepoint/1000)) in v_meta["scenes"])
 			{
-				scene_count += 1;
-				curr_scene = v_meta["scenes"]["elem_" + (cuepoint/1000)];
-				curr_demo_value = $('#demo_info_box').html()
-				$('#demo_info_box').html(curr_demo_value + '<span id="elem_' + (cuepoint/1000) + '" style="display:none;">' + 
-					'Scene ' + scene_count + ': ' + curr_scene["startTime"] + " - " + curr_scene["stopTime"] + "<br />" + 
-					'Top Keyword: ' + curr_scene["keywords"][0]["keyword"] + '&nbsp; &nbsp; &nbsp; ' + 'Top Category: ' + curr_scene["categories"][0]["category"] + "<br />" + 
-					'&nbsp;&nbsp;Relevance: ' + curr_scene["keywords"][0]["relevance"] + '&nbsp; &nbsp; &nbsp; Relevance: ' + curr_scene["categories"][0]["relevance"] + 
-					'</span><br /><br />');
-				$('#elem_' + (cuepoint/1000)).fadeIn();
-				$('#demo_info_box').scrollTop(9999999);
+				printDemoText(cuepoint/1000,"none");
 			}
           });
       } 
    }
 });
+
+function setDemoTextToTime(new_cuetime) {
+	window.scene_count = 0;
+	console.log("in new method");
+	updated_content = getDemoBoxHeader("inline");
+	for (curr_time = 0; curr_time <= new_cuetime; curr_time += 1000)
+	{
+		if ( ("elem_" + (curr_time/1000)) in window.v_meta["scenes"])
+		{
+			if (window.v_meta["scenes"]["elem_" + (curr_time/1000)]["keywords"].length > 0) //Make sure there are some keywords to be had.
+			{
+				window.scene_count += 1;
+				updated_content += getDemoBoxText(curr_time/1000);
+			}
+		}
+	}
+	console.log("in new method - 2");
+	$('#demo_info_box').html( updated_content );
+	console.log("in new method - 3");
+	/*$('#initial_demo_text').toggle(function() {
+		$('#demo_info_box').find("span").css('display','none').toggle();
+	});*/
+	console.log("in new method - 4");
+	$('#demo_info_box').scrollTop(9999999);
+}
 
 function getCuepoints(clip) {
   var cuepoints = new Array();
@@ -58,15 +80,49 @@ function getCuepoints(clip) {
 }
 
 $(function() {
-	initial_timer = setTimeout('setupDemoText();', 1000);
+	initial_timer = setTimeout('setupDemoText("none");', 1000);
 });
 
-function setupDemoText() {
+function setupDemoText(display) {
 	v_meta = window.video_metadata["video_metadata"];
-	$('#demo_info_box').html('<span id="initial_demo_text" style="display:none;">' + 
+	$('#demo_info_box').html(getDemoBoxHeader(display));
+	$('#initial_demo_text').fadeIn();
+}
+
+function getDemoBoxHeader(display) {
+	v_meta = window.video_metadata["video_metadata"];
+	return ('<span id="initial_demo_text" style="display:'+display+';">' + 
 		v_meta["title"] + 
 		'<br />Title: ' + 
 		v_meta["videoName"] + 
-		'</span><br /><br />');
-	$('#initial_demo_text').fadeIn();
+		'</span><br /><div style="width:300px;padding:0 0 0 4px;"><hr /></div><br />');
+}
+
+function getDemoBoxText(curr_cuepoint, display) {
+	curr_scene = window.video_metadata["video_metadata"]["scenes"]["elem_" + curr_cuepoint];
+	if (curr_scene["keywords"].length <= 0) //Make sure there are some keywords to be had.
+	{
+		return ('<span id="elem_' + curr_cuepoint + '" style="display:none;" />');
+	}
+	relevance_break = "";
+	for (i=0; i < curr_scene["keywords"][0]["keyword"].length; i++)
+		if (i > 6)
+			relevance_break += "&nbsp;";
+	curr_demo_value = '<span id="elem_' + curr_cuepoint + '" style="display:'+display+';">' + 
+		'<strong>Scene ' + window.scene_count + ':</strong> ' + curr_scene["startTime"] + " - " + curr_scene["stopTime"] + "<br />" + 
+		'<div style="display:inline-block;">' + 
+		'<div style="display:inline-block;">Top Keyword: ' + curr_scene["keywords"][0]["keyword"] + '<br />Relevance: ' + curr_scene["keywords"][0]["relevance"] + '</div>' + 
+		'<div style="display:inline-block;padding-left:20px;">Top Category: ' + curr_scene["categories"][0]["category"] + '<br />Relevance: ' + curr_scene["categories"][0]["relevance"] + '</div>' + 
+		'</div>' + 
+		'</span><br /><br />';
+	return (curr_demo_value);
+}
+
+function printDemoText(curr_cuepoint, display) {
+	window.scene_count += 1;
+	curr_scene = window.video_metadata["video_metadata"]["scenes"]["elem_" + curr_cuepoint];
+	curr_demo_value = $('#demo_info_box').html();
+	$('#demo_info_box').html( curr_demo_value + "" + getDemoBoxText(curr_cuepoint,display) );
+	$('#elem_' + curr_cuepoint).fadeIn();
+	$('#demo_info_box').scrollTop(9999999);
 }
